3ee62df5440673c3ea99bbe673eaee70a0ff884c
Ken Holston <git.kholston@gmail.com>
2024-12-17 00:14:54 -0500

    create mock data and resolvers

---
+++ next-app/refactored-app/src/graphql/data.ts
@@ -0,0 +1,23 @@
+export const db = [
+    {
+        zip: "96815",
+        weather: "sunny",
+        tempC: "25C",
+        tempF: "70C",
+        friends: ["96814", "96826"]
+    },
+    {
+        zip: "96826",
+        weather: "sunny",
+        tempC: "30C",
+        tempF: "86C",
+        friends: ["96814", "96815"]
+    },
+    {
+        zip: "96814",
+        weather: "sunny",
+        tempC: "20C",
+        tempF: "68C",
+        friends: ["96815", "96826"]
+    },
+]
