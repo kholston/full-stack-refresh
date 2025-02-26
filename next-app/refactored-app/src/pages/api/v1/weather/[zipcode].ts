9312e1dc900eb68f08b64fb63fc4d1eb98334155
Ken Holston <git.kholston@gmail.com>
2025-02-26 00:23:31 -0500

    add mongodb database connection

--- next-app/refactored-app/src/pages/api/v1/weather/[zipcode].ts
+++ next-app/refactored-app/src/pages/api/v1/weather/[zipcode].ts
@@ -1,12 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { findByZip } from "../../../../../mongoose/weather/services";
+import dbConnect from "../../../../../middleware/db-connect";
+
+dbConnect();
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ): Promise<NextApiResponse<WeatherDetailType> | void> {
-    return res.status(200).json({
-        zipcode: req.query.zipcode,
-        weather: "sunny",
-        temp: 35
-    })
+
+    let data = await findByZip(req.query.zipcode as string);
+    return res.status(200).json(data)
 }
